84769033a06d950b31e72fece41a818b8234120c
arthureric <founder.mun@gmail.com>
2017-08-09 16:08:37 +0900

    add 02-data-type-1.py

---
+++ 02-data-type-1.py
@@ -0,0 +1,42 @@
+# ctrl + /  블럭 주석처리, 주석 해제
+
+#### 정수 int - integer
+print(1+1)
+
+####소수 float
+print(1.5+2.1)
+
+####문자 str - string
+print("hello")
+
+####문자열 더하기
+print("hello" + "1")
+
+####정수 소수간 변경
+print(4/2) # 2.0
+print(4//2) # 2
+print(5/3) # 1.666666666667
+print(5//3) # 1  (몫)
+print(7//2) # 3
+print(5%3) # 2   (나머지)
+print(7%2) # 1
+
+print(4//2)
+print(int(4/2))
+# 정수 정수
+
+
+#정수를 소수로 하고 싶다.
+print(float(2))
+
+
+#### 문자열 숫자간 변경
+print("1")
+#문자를 숫자로 바꾸소 싶다면, print(int("1"))
+
+
+print(int("1.5"))
+# # 1) 1
+# print(int(1.5))
+# 2) error
+# 문자열에 소수가 있을때, int로 변경시 오류가 난다.
