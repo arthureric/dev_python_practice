132370d7135bbc5fdbdaf4508d388e42176985b4
arthureric <founder.mun@gmail.com>
2017-08-11 17:50:06 +0900

    {

---
+++ 06-multiplication-table-2.py
@@ -0,0 +1,8 @@
+
+number = input("몇 단을 출력하시겠습니까?")
+print(number, "단")
+number = int(number)
+
+for b in range(1, 10):
+
+    print(number, "*", b, "=",  number * b)
---
+++ 06-multiplication-table.py
@@ -0,0 +1,54 @@
+
+
+
+
+
+number = input("몇 단을 출력하시겠습니까? ")
+print(number, "단")
+# print("몇 단을 출력하시겠습니까?")
+
+#
+# # a = 숫자다
+#
+# a = x
+# b = num in range(1,10)
+#
+#
+# # a에 숫자를 넣으면,
+#
+# if x = num in range(1,0)
+# while b<10:
+#     print ("a" "*" "b"= int("a*b"))
+
+
+#     a = range(1, 10)
+#     b = 0
+#
+#
+#
+# while b<10 :
+#     b = b+1
+#     # print(b)
+#     print('a*b=int(a*b)')
+#
+# list_a =[1,2,3,4,5,6,7,8,9]
+# # a = list_a[n]
+# sample(range(1,10), 1)
+#
+#
+# n = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+number = int(number)
+
+for b in range(1, 10) :
+    # print(b)
+    # while b<10:
+    #     b=b+1
+    #     print(number, b)
+
+
+
+# b = int()
+# while b<10:
+#     b=b+1
+#
+    print(number * b)
